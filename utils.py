af116305ce80d22709eaf6808d9dbb637fbca15d
BenKet <benedict.kettler@gmail.com>
2024-04-22 15:54:41 +0200

    just male

--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ def load_titanic_female():
     df = pd.read_csv('data/titanic.csv')
     print(df.columns)
     print(df["sex"].unique())
-    female_df = df[df["sex"]=="female"]
+    female_df = df[df["sex"]=="male"]
     return female_df
